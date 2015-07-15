584c735492b274abcae79fe6aecb2f478a391da4
Rodrigo Monteiro Ferreira <rodrigomonteirof@gmail.com>
2015-07-15 14:21:11 -0300

    Add routes description in Read me file

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -2,7 +2,6 @@ class MapsController < ApplicationController
   before_action :set_values, only: [:best_route]
   skip_before_action :verify_authenticity_token
 
-  # curl --data "name=SP&routes[]=A B 10&routes[]=B D 15&routes[]=A C 20&routes[]=C D 30&routes[]=B E 50&routes[]=D E 30" http://localhost:3000/maps
   def create
     @map = Map.find_or_create_by(name: map_params['name'])
     @map.load_routes(map_params['routes'])
@@ -14,7 +13,6 @@ class MapsController < ApplicationController
     end
   end
 
-  # curl -isS "localhost:3000/bestroute/SP/A/D/9.5/250"
   def best_route
     router = RouterService.new(@map)
     route = router.shortest_path(params['origin'], params['destiny'])
