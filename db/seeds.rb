f937d10be4e3d33d22a25de38e328f394b9270e4
Rodrigo Monteiro Ferreira <rodrigomonteirof@gmail.com>
2015-07-15 14:41:05 -0300

    add seed with default values

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Map.new(name: 'SIMPLE').save
+m = Map.find_by(name: 'SIMPLE')
+
+Route.new(map: m, origin: 'A', destiny: 'B', distance: 10).save
+Route.new(map: m, origin: 'B', destiny: 'D', distance: 15).save
+Route.new(map: m, origin: 'A', destiny: 'C', distance: 20).save
+Route.new(map: m, origin: 'C', destiny: 'D', distance: 30).save
+Route.new(map: m, origin: 'B', destiny: 'E', distance: 50).save
+Route.new(map: m, origin: 'D', destiny: 'E', distance: 30).save
